64fa1c89d90095be7045c32b62c7522f08aa23b2
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-25 21:21:39 +0200

    Profile Reducer & Get Current Profile

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = () => {
+  const auth = useSelector((state) => state.auth);
+  const profile = useSelector((state) => state.profile);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentProfile());
+  }, []);
+
   return <div className='container'>dashboard</div>;
 };
 
