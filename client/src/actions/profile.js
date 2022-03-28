9aacad6eca60853b0ff1a9905358fd75b2b38ca8
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-28 12:21:36 +0300

    Get all users, get users by id

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,6 +7,7 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   DELETE_ACCOUNT,
+  GET_PROFILES,
 } from './types';
 
 // Get current users profile
@@ -16,6 +17,48 @@ export const getCurrentProfile = () => async (dispatch) => {
 
     dispatch({
       type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.status.text,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Get All Profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get('/api/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.status.text,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// GET profile by id
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/user${userId}`);
+
+    dispatch({
+      type: GET_PROFILES,
       payload: res.data,
     });
   } catch (err) {
